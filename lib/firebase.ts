5bcc406b9d0e315986e66181a825a00978c35af2
saddam <saddam.saqqar@icloud.com>
2026-07-12 14:11:45 +0300

    رفع المشروع

---
+++ lib/firebase.ts
@@ -0,0 +1,23 @@
+// firebase.ts
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  // Paste config here
+  apiKey: "AIzaSyDbqRHcS5xozE26ANS5RxYGjnQMlwwb8gM",
+  authDomain: "asaad-dobae.firebaseapp.com",
+  databaseURL: "https://asaad-dobae-default-rtdb.firebaseio.com",
+  projectId: "asaad-dobae",
+  storageBucket: "asaad-dobae.firebasestorage.app",
+  messagingSenderId: "879581253100",
+  appId: "1:879581253100:web:7b793aeb3cd11c593e04c9",
+  measurementId: "G-GMX3ZBRL5R"
+};
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const database = getDatabase(app);
+
+export { auth, db, database };
